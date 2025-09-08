e0c469d68e4b28f55d0c376cad48675089342de0
Francisco Ramón Castro García <framon.98@outlook.com>
2025-09-08 09:57:37 -0600

    Add header javascript file

--- components/header.js
+++ components/header.js
@@ -2,6 +2,21 @@ class Header extends HTMLElement {
     constructor() {
         super();
     }
+
+    connectedCallback(){
+        this.innerHTML = `
+        <header class="container">
+            <div class="row">
+                <h1 class="col-sm-6"><a href="index.html">Francisco R Castro G</a></h1>
+                <nav class="col-sm-6 text-right">
+                <a href="about.html">About Me</a>
+                <a href="education.html">Education</a>
+                <a href="experience.html">Experience</a>
+                </nav>
+            </div>
+        </header>
+        `;
+    }
 }
 
 customElements.define('header-component', Header);
